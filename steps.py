40812b22d8a5d5650bdad8a67ad4087ec9740d22
AgosRobledo <131928579+AgosRobledo@users.noreply.github.com>
2024-10-23 21:34:24 -0300

    Update steps.py
    pytest

--- steps.py
+++ steps.py
@@ -1,55 +1,27 @@
-from behave import given, when, then
-from tiendaonline2024 import agregar_al_carrito, carrito, productos
+import pytest
+import sys
+sys.path.append("C:/Users/Usuario/Documents/2do/Programación/PYTHON/Carrito2024")
+from tiendaonline2024 import productos, carrito, agregar_al_carrito, ver_carrito, validar_opcion, buscarProducto
+from unittest import mock
 
 
+def test_buscar_producto_existente(capfd):
+    # Busca un producto que existe
+    buscarProducto('001', productos)
+    
+    # Captura la salida de la función
+    out, err = capfd.readouterr()
+    
+    assert "Producto encontrado:" in out
+    assert "Iphone 13 mini" in out
 
-@given('el carrito está vacío')
-def step_impl(context):
-    carrito.clear()
+def test_buscar_producto_no_existente(capfd):
+    # Busca un producto que no existe
+    buscarProducto('999', productos)
+    
+    # Captura la salida de la función
+    out, err = capfd.readouterr()
 
-@given('el stock del producto "{codigo}" es {stock:d}')
-def step_impl(context, codigo, stock):
-    productos[codigo]["stock"] = stock
-
-@when('agrego {cantidad:d} unidades del producto "{codigo}" al carrito')
-def step_impl(context, cantidad, codigo):
-    agregar_al_carrito(carrito, codigo, cantidad)
-
-@then('el carrito debe contener el producto "{codigo}"')
-def step_impl(context, codigo):
-    assert codigo in carrito
-
-@then('la cantidad del producto "{codigo}" debe ser {cantidad:d}')
-def step_impl(context, codigo, cantidad):
-    assert carrito[codigo]["cantidad"] == cantidad
-
-@given('el carrito tiene {cantidad:d} unidades del producto "{codigo}"')
-def step_impl(context, cantidad, codigo):
-    agregar_al_carrito(carrito, codigo, cantidad)
-
-@when('intento agregar {cantidad:d} unidades del producto "{codigo}"')
-def step_impl(context, cantidad, codigo):
-    agregar_al_carrito(carrito, codigo, cantidad)
-
-@then('el carrito no debe contener el producto "{codigo}"')
-def step_impl(context, codigo):
-    assert codigo not in carrito
-
-@then('el carrito debe tener más de 0 productos')
-def step_impl(context):
-    assert len(carrito) > 0
-
-@when('cambio la cantidad del producto "{codigo}" a {nueva_cantidad:d}')
-def step_impl(context, codigo, nueva_cantidad):
-    carrito[codigo]["cantidad"] = nueva_cantidad
-
-@when('elimino el producto "{codigo}" del carrito')
-def step_impl(context, codigo):
-    del carrito[codigo]
-
-@then('el carrito debe estar vacío')
-def step_impl(context):
-    assert len(carrito) == 0
-
+    assert "El producto que desea buscar no existe" in out
 
 
